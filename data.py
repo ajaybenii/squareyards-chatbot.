3ee827010dcadbcd83233bf33e7d597b14d23459
ajaybenii <ajayben4698@gmail.com>
2025-05-14 17:28:40 +0530

    update main

--- data.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017")
-db = client["squareyards"]
-collection = db["property_listings"]
-collection.insert_one({"test": "data"})
-print(list(collection.find()))
